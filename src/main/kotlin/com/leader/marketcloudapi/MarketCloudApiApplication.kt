a2ba532364ed1a182d43d26d99c6f21850663de1
wuhaozhen <wuhaozhen1106@gmail.com>
2021-11-18 22:14:21 -0600

    Initial Commit

---
+++ src/main/kotlin/com/leader/marketcloudapi/MarketCloudApiApplication.kt
@@ -0,0 +1,11 @@
+package com.leader.marketcloudapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MarketCloudApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<MarketCloudApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/leader/marketcloudapi/MarketCloudApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.leader.marketcloudapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MarketCloudApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
